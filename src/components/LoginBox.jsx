5cef468b1a7067d309ea77a5c08815fe6fb41755
arshaqV <arshaq.assoo@gmail.com>
2024-01-14 22:32:57 +0530

    Fix animation order and add Firebase auth

--- src/components/LoginBox.jsx
+++ src/components/LoginBox.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 const root = document.getElementById("root")
 root.className = "loginClass"
 
@@ -7,15 +9,26 @@ const LoginBox = () => {
 
     const navigate = useNavigate();
     const handleClick = () => {
-    loginEvent()
-    setTimeout(() => {
-      navigate('/chat');
-    }, 300);
+        const auth = getAuth();
+        const email = document.getElementById("username").value+"@noreply.smalltalk.io"
+        console.log(email)
+        const password = document.getElementById("password").value
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        navigate("/chat")
+      })
+      .catch((error) => {
+        console.log(error.code);
+        console.log(error.message);
+      }); 
+    
     };
 
     return ( <div id="loginForm">
-        <input type="text" placeholder="Username"></input>
-        <input type="password" placeholder="Password"></input>
+        <input type="text" id="username" placeholder="Username"></input>
+        <input type="password" id="password" placeholder="Password"></input>
         <button className="buttonBox" onClick={handleClick}>Login</button>
         <br/>
         <div className="divider"></div>
@@ -24,30 +37,8 @@ const LoginBox = () => {
 }
 
 let loginEvent = function() {
-    const logo = document.getElementById("smalltalk")
-    const line = document.getElementById("headerLine")
-    const form = document.getElementById("loginForm")
-    //const rect = logo.getBoundingClientRect();
-    form.style.opacity = "0"
-    logo.style.opacity = "0"
-    //logo.style.top = -rect.top+20+"px"
-    logo.style.top = "-80px"
-    line.style.width = "100vw"
-    line.style.opacity = "100%"
-    setTimeout(()=>{
-        const container = document.getElementById("mainContainer")
-        logo.style.position="absolute"
-        logo.style.top="20px"
-        logo.style.opacity="100%"
-        line.style.transition="all 0s"
-        container.style.opacity = "100%"
-        container.style.visibility ="visible"
-        form.remove()
-    },400)
-
-    setTimeout(()=>{
-          root.className = ""
-    },400)
+    
 }
  
+
 export default LoginBox;
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,11 +1,41 @@
+import { useEffect } from "react";
 import ChatList from "./ChatList";
 import { Outlet } from "react-router-dom";
 
 const MainContainer = () => {
+    useEffect(onCreateAnimation)
     return ( <div id="mainContainer">
     <ChatList />
     <Outlet />
    </div> );
 }
  
+const onCreateAnimation = function() {
+    const logo = document.getElementById("smalltalk")
+    const line = document.getElementById("headerLine")
+    const form = document.getElementById("loginForm")
+    const container = document.getElementById("mainContainer")
+    //const rect = logo.getBoundingClientRect();
+    form.style.opacity = "0"
+    logo.style.opacity = "0"
+    //logo.style.top = -rect.top+20+"px"
+    logo.style.top = "-80px"
+    line.style.width = "100vw"
+    line.style.opacity = "100%"
+    setTimeout(()=>{
+        
+        logo.style.position="absolute"
+        logo.style.top="20px"
+        logo.style.opacity="100%"
+        line.style.transition="all 0s"
+        container.style.position="static"
+        container.style.opacity = "100%"
+        container.style.visibility ="visible"
+        form.remove()
+    },400)
+
+    setTimeout(()=>{
+          root.className = ""
+    },400)
+}
 export default MainContainer;
