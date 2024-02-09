1e714a6aaeeb983d9b9dab196b8fa2785487c29e
arshaqV <arshaq.assoo@gmail.com>
2024-02-10 02:21:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/smalltalk/'
+  base: 'https://arshaqv.github.io/smalltalk/'
 })
